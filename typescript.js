b3a37e275690d27160a7240d7323abe7c21ff661
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-21 15:31:44 +0900

    finished the first part of learning

---
+++ typescript.js
@@ -0,0 +1,22 @@
+// const sum = (a : number, b : number ) => {
+//   return a + b;
+// }
+// const answer = sum(5, 5)
+// console.log(answer)
+//boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = "brown";
+var favouriteQuote = "I'm not old, i'm only " + age;
+// arrays
+var pets = ["cat", "dogs", "pig"];
+var pets2 = ["lion", "tiger"];
+// object
+var wizard = {
+    a: "John"
+};
+//null and undefined
+var meh = undefined;
+var noo = null;
--- typescript.ts
+++ typescript.ts
@@ -1,3 +1,29 @@
-function sum(a,b) {
-  return a + boolean;
-}
+// const sum = (a : number, b : number ) => {
+//   return a + b;
+// }
+
+// const answer = sum(5, 5)
+// console.log(answer)
+
+//boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = "brown";
+let favouriteQuote: string = `I'm not old, i'm only ${age}`;
+
+// arrays
+let pets: string[] = ["cat", "dogs", "pig"];
+let pets2: Array<string> = ["lion", "tiger"];
+
+// object
+let wizard: object = {
+  a: "John",
+};
+
+//null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
