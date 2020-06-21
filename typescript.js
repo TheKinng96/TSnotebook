e0e5dd3e30105ea27fde7b56b210692a769da54b
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-21 15:51:29 +0900

    finished the second part of learning

--- typescript.js
+++ typescript.js
@@ -20,3 +20,38 @@ var wizard = {
 //null and undefined
 var meh = undefined;
 var noo = null;
+// new feature
+// tuple
+var basket;
+basket = ["ball", 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
+// Any
+var whatever = "aghhhhhh noooo!!!";
+whatever = Size.Small;
+whatever = basket; // use any when converting js to ts while dont really care about what type it is
+// void  => no return, doesnt return anything
+var sing = function () {
+    console.log("lalalalalala");
+};
+var dance = function () {
+    var a = "John";
+    console.log("dance");
+    return a;
+};
+// never : testing 1.a function never return 2.doesnt have an endpoint
+var error = function () {
+    throw Error("oooops");
+};
+var fightRobotArmy = function (robots) {
+    console.log("FIGHT!");
+};
+var fightRobotArmy2 = function (robots) {
+    console.log("FIGHT!");
+};
--- typescript.ts
+++ typescript.ts
@@ -27,3 +27,53 @@ let wizard: object = {
 //null and undefined
 let meh: undefined = undefined;
 let noo: null = null;
+
+// new feature
+// tuple
+let basket: [string, number];
+basket = ["ball", 5];
+
+// Enum
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
+let sizeName: string = Size[2];
+
+// Any
+let whatever: any = "aghhhhhh noooo!!!";
+whatever = Size.Small;
+whatever = basket; // use any when converting js to ts while dont really care about what type it is
+
+// void  => no return, doesnt return anything
+let sing = (): void => {
+  console.log("lalalalalala");
+};
+
+let dance = (): string => {
+  var a: string = "John";
+  console.log("dance");
+  return a;
+};
+
+// never : testing 1.a function never return 2.doesnt have an endpoint
+let error = (): never => {
+  throw Error("oooops");
+};
+
+// interface
+interface RobotArmy {
+  count: number;
+  type: string;
+  magic: string;
+}
+
+let fightRobotArmy = (robots: RobotArmy) => {
+  console.log("FIGHT!");
+};
+let fightRobotArmy2 = (
+  robots: { count: number; type: string; magic: string },
+) => {
+  console.log("FIGHT!");
+};
